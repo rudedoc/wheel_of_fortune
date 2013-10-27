dfaa26a32f8210909431c95f5ec12278c44d2cf9
rudedoc <finlay.mark@gmail.com>
2013-10-27 22:52:58 +0000

    depoly fixes

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'quiet_assets'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 gem 'will_paginate'
-gem 'devise', git: 'http://github.com/plataformatec/devise.git', branch: 'master'
+gem 'devise', github: 'plataformatec/devise.git', branch: 'master'
 gem 'activerecord-session_store', github: 'rails/activerecord-session_store'
 
 gem 'rails_12factor', group: :production
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -138,8 +138,8 @@ function create_spin() {
 
     $.ajax({
         type: "POST",
-        url: "http://localhost:3000/spins.json",
-//        url: "http://evening-castle-3789.herokuapp.com/spins.json",
+//        url: "http://localhost:3000/spins.json",
+        url: "http://evening-castle-3789.herokuapp.com/spins.json",
         cache: false,
         data: new_spin,
         dataType: "json",
@@ -201,8 +201,8 @@ function send_result() {
 
     $.ajax({
         type: "PUT",
-        url: "http://localhost:3000/spins/" + current_spin_id + ".json",
-//        url: "http://evening-castle-3789.herokuapp.com/spins/" + current_spin_id + ".json",
+//        url: "http://localhost:3000/spins/" + current_spin_id + ".json",
+        url: "http://evening-castle-3789.herokuapp.com/spins/" + current_spin_id + ".json",
         cache: false,
         data: update_spin,
         dataType: "json"
